c877bbe805ed7b0ee42633b10273e74e0e2dbe39
Alberto <albertomaronezio@hotmai.com>
2019-05-29 15:01:56 -0300

    Exercicio_Parte2_07

---
+++ Exercicio_Parte2_08/Exercicio_Parte2_08/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Parte2_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeDigitado = string.Empty;
+
+            Console.Write("Digite um nome: ");
+            nomeDigitado = Console.ReadLine();
+
+            if (nomeDigitado == "Alberto")
+            {
+                Console.WriteLine("NOME CORRETO");
+            }
+
+            else
+            {
+                Console.WriteLine("NOME INCORRETO");
+            }
+        }
+    }
+}
